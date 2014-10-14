700e1c778a4b0e884f2541366f7cb3939d1e1c11
Kelvin <kaft.xz003@gmail.com>
2014-10-14 18:23:26 -0500

    14102014 6:23

--- DEMO/STATIC/JS/script.js
+++ DEMO/STATIC/JS/script.js
@@ -1,5 +1,35 @@
 var app=angular.module("app",[]);
-  
+ 
 function PruebaController($scope) {
-  $scope.mensaje="Hola Mundo";
+	$scope.mensaje="Hola Mundo";
 }
+
+function HoraControler ($scope) {
+	var updateclock = function(){
+		var h = new Date()
+		$scope.clock = h.getHours()+
+			':'+h.getMinutes()+
+			':'+h.getSeconds();
+	};
+	var timer = setInterval( 
+		function(){
+			$scope.$apply(updateclock)
+		},1000);
+	updateclock();
+}
+
+function ContController($scope){
+	$scope.cont = 0
+	$scope.add = function(parm){
+		$scope.cont += parm;
+	};
+	$scope.subtract = function(parm){
+		$scope.cont -= parm; 
+	};
+}
+// var app=angular.module("app",[]) // <-- This is the getter function for a module previously defined
+// .run(function($rootScope) {
+// $rootScope.user = {
+// email: 'ari@fullstack.io'
+// }
+// });
